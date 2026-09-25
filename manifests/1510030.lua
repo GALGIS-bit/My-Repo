04498970d2a711875d4865ef2ba702ab497ff041
GALGIS-bit <dn135306@gmail.com>
2026-09-25 09:40:15 +0700

    i guess bruh

---
+++ manifests/1510030.lua
@@ -0,0 +1,11 @@
+-- ============================================================
+-- Descargado desde Walftech  ->  https://walftech.com
+-- Generador de Manifest y Lua
+-- AppID: 1510030
+-- Fecha: 2026-09-25 02:31 UTC
+-- ============================================================
+
+--Gamename Knights College
+addappid(1510030) --Mainappid Knights College
+addappid(1510031, 1, "4b6d4839e187ee85a9106864e0daa9812fd1c5615463465fe6b2d5880cee9552") --Main Windows Depot Knights College
+setManifestid(1510031, "144636852970103784", 1255445184)
